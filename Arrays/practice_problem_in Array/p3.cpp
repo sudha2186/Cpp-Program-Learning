293f5e633667759b6ff158c5fe5964fad0e51e55
sudha2186 <rajendramaurya46223@gmail.com>
2025-05-29 18:48:25 +0530

    Added some medium level question

--- Arrays/practice_problem_in Array/p3.cpp
+++ Arrays/practice_problem_in Array/p3.cpp
@@ -1,17 +1,33 @@
-// Print all the unique values in array
 #include <iostream>
 using namespace std;
 
 void uniqueVal(int arr[], int n)
 {
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
-        
+        bool isUnique = true;
+
+        for (int j = 0; j < n; j++)
+        {
+            if (i != j && arr[i] == arr[j])
+            {
+                isUnique = false;
+                break;
+            }
+        }
+
+        if (isUnique)
+        {
+            cout << arr[i] << " ";
+        }
     }
+    cout << endl;
 }
-int main() {
+
+int main()
+{
     int size = 6;
-    int arr[] = {1, 2, 4, 2, 3, 1,};
-    uniqueVal(arr, size);   
+    int arr[] = {1, 2, 4, 2, 3, 1};
+    uniqueVal(arr, size);
     return 0;
 }
