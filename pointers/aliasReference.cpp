a563260fd92b9230e1d960fc729f1b8d896e8d70
Sudha Maurya <rajendramaurya46223@gmail.com>
2025-05-30 16:24:16 +0530

    Update aliasReference.cpp

--- pointers/aliasReference.cpp
+++ pointers/aliasReference.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 
-void changeA(int &b) // pass by reference is done using alias(another name of parameter) or refernces due thid also changes will be done in main func
+void changeA(int &b) // pass by reference is done using alias(another name of parameter) or refernces due to this, changes will also be done in main func
 {
-    b = 70; // here in memory new copy of a is not created directly new pamter is created where address of a is stored
+    b = 70; // here in memory new copy of a is not created, directly new pamter is created where address of a is stored
 }
 
 int main() {
