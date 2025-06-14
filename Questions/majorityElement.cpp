d24128e7a5568e0915ed069afec939179ed560c5
sudha2186 <rajendramaurya46223@gmail.com>
2025-06-14 17:20:17 +0530

    Majority element brute firce approach

---
+++ Questions/majorityElement.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+// Time complexity O(n^2)
+int majorityEle(vector<int> nums)
+{
+    for(int val : nums) {
+        int freq = 0;
+        for(int ele : nums) {
+            if(val == ele) freq++;
+        }
+        if(freq > nums.size() / 2) return val;
+    }
+}
+
+int main() {
+    vector<int> nums = {1, 2, 2, 1, 1};
+    int ele = majorityEle(nums);
+    cout << ele << endl;
+    return 0;
+}
