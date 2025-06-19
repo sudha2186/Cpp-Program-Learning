e45743e25d996e851773151bca8e103b2b2a749a
sudha2186 <rajendramaurya46223@gmail.com>
2025-06-19 23:57:22 +0530

    updated

--- strings/basics2.cpp
+++ strings/basics2.cpp
@@ -7,6 +7,12 @@ int main() {
     string str2 = "college";
     cout << str1 + str2 << endl; // concatenation
     cout << (str1 == str2) << endl; // comparison
-    
+    cout << str1.length() << endl; // length
+
+    string str;
+    // cin >> str; // ex: hello world => output: hello 
+    getline(cin, str);
+    cout << "output: " << str << endl;  
+
     return 0;
 }
