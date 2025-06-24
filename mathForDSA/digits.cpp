cac5703d81c276ecb7e9db165581122b448fb4fb
sudha2186 <rajendramaurya46223@gmail.com>
2025-06-25 00:41:22 +0530

    updated

--- mathForDSA/digits.cpp
+++ mathForDSA/digits.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
+// TC => O(log n) base 10 
+
+void printDigit(int n) 
+{
+    int dig;
+    while(n != 0) {
+        dig = n%10;
+        cout << dig << " ";
+        n /= 10; // because of this division by 10 B-10
+    }
+    cout << endl;
+}
 
 int main() {
     int n = 4567;
-    int dig;
-    while(n > 0) {
-        dig = n%10;
-        cout << dig << " ";
-        n /= 10;
-    }
+    printDigit(n);
+    cout << (int)(log10(n) + 1) << endl; // to count the digit (trick)
     return 0;
 }
