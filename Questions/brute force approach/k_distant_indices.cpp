af6de9a23bd031613ba05102122b904473e646c6
sudha2186 <rajendramaurya46223@gmail.com>
2025-06-25 00:03:32 +0530

    k distant indices prblm

--- Questions/brute force approach/k_distant_indices.cpp
+++ Questions/brute force approach/k_distant_indices.cpp
@@ -1,22 +1,29 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
-        vector<int> res;
-        int n = nums.size();
-        // traverse number pairs
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (nums[j] == key && abs(i - j) <= k) {
-                    res.push_back(i);
-                    break;  // early termination to prevent duplicate addition
-                }
+    vector<int> res;
+    int n = nums.size();
+    // traverse number pairs
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (nums[j] == key && abs(i - j) <= k) {
+                res.push_back(i);
+                break;  // early termination to prevent duplicate addition
             }
         }
-        return res;
-    } 
-    
+    }
+    return res;
+} 
+
 int main() {
-    
+    vector<int> nums = {3,4,9,1,3,9,5};
+    int key = 9, k = 1;
+    vector <int> ans = findKDistantIndices(nums, key, k);
+    for(int val : ans) {
+        cout << val << " ";
+    }
+    cout << endl;
     return 0;
 }
