51ab248bcdf38d0c1c9a097627c7902749ed3bfb
sudha2186 <rajendramaurya46223@gmail.com>
2025-06-25 23:47:49 +0530

    updated

--- mathForDSA/gcd.cpp
+++ mathForDSA/gcd.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int gcd(int a, int b) 
+int gcd(int a, int b)
 {
     if(b == 0) return a;
-
     return gcd(b, a%b);
 }
+
+string gcdOfStrings(string str1, string str2) 
+{
+    int gcdlen = gcd(str1.length(), str2.length());
+    
+    if(str1 + str2 != str2 + str1) return "";
+    return str1.substr(0, gcd(str1.length(), str2.length()));
+}
+
 int main() {
-    int a = 0, b = 24;
-    cout << gcd(a, b) << endl;
+    string str1 = "ABCABC", str2 = "ABC";
+    cout << gcdOfStrings(str1, str2) << endl;
     return 0;
 }
