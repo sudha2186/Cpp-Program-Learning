b46b77fe5cb47b9d1198f01d21dcb2c13c116447
sudha2186 <rajendramaurya46223@gmail.com>
2025-07-10 22:58:01 +0530

    updated

--- sorting/bubbleSort.cpp
+++ sorting/bubbleSort.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
 using namespace std;
+// TC => o(n^2)
+void bubbleSort(int arr[], int n) {
+    bool swapped;
+    for (int i = 0; i < n-1; i++) {
+        swapped = false;
+
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                // swap
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                swapped = true;
+            }
+        }
+
+        // If no two elements were swapped in inner loop, break
+        if (!swapped)
+            break;
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
 
 int main() {
-    
+    int arr[] = {5, 1, 4, 2, 8};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    cout << "Original array: ";
+    printArray(arr, n);
+
+    bubbleSort(arr, n);
+
+    cout << "Sorted array: ";
+    printArray(arr, n);
+
     return 0;
 }
