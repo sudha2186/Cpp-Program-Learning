2b907e5ff2dc8b6ef59e6c8e4486b5a744ead2fa
sudha2186 <rajendramaurya46223@gmail.com>
2025-07-13 16:17:49 +0530

    book allocator problem

---
+++ Questions/optimalApproachProblem/bookAllocator.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+// same problem as painter partition 
+//TC => O(n logn); sc => O(1)
+bool isValid(vector<int> arr, int n, int m, int max) {
+    int stu = 1, pages = 0;
+    for(int i = 0; i < n; i++) {
+        if(arr[i] > max) return false;
+
+        if(pages + arr[i] <= max) {
+            pages += arr[i];
+        } else {
+            stu++;
+            pages = arr[i];
+            if(stu > m) return false;  // ✅ critical fix
+        }
+    }
+    return true;
+}
+
+
+int allocate(vector<int> arr, int n, int m)
+{
+    int sum = 0;
+    for(int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+    int st = 0, end = sum;
+    int ans = -1;
+    while (st <= end)
+    {
+        int mid = st + (end - st)/2;
+        if(isValid(arr, n, m, mid)) {
+            ans = mid;
+            end = mid - 1;
+        } else {
+            st = mid + 1;
+        }
+    }
+    return ans;
+}
+
+int main() {
+    vector<int> arr = {15, 17, 20};
+    int n = 3, m = 2;
+    cout << allocate(arr, n, m);
+    return 0;
+}
