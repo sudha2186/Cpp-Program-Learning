f42eb805acbf2d0a0883009bd583f2cc7172eb07
sudha2186 <rajendramaurya46223@gmail.com>
2025-07-14 23:42:09 +0530

    updated

---
+++ Questions/optimalApproachProblem/AggressiveCows.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int posOfCows(vector<int> arr, int n, int m)
+{
+
+}
+
+int main() {
+     vector<int> arr = {15, 17, 20};
+    int n = 3, m = 2;
+    cout << posOfCows(arr, n, m);
+    return 0;
+}
