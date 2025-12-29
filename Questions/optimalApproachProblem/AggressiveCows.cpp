f021112946b006b6d001ba6736bf595d2af1c6f8
sudha2186 <rajendramaurya46223@gmail.com>
2025-12-29 17:47:34 +0530

    painter partition problem variation

--- Questions/optimalApproachProblem/AggressiveCows.cpp
+++ Questions/optimalApproachProblem/AggressiveCows.cpp
@@ -1,15 +1,42 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-int posOfCows(vector<int> arr, int n, int m)
-{
+// T.C => O(nlogN) 
+// Similar to painter partition but here we need to find largest minimum distance opposite of painter partition problem
+bool isPossible(vector<int> &arr, int n, int m, int mid) {
+    int cows = 1, lastStallPos = arr[0];
+    for(int i = 0; i < n; i++) {
+        if(arr[i] - lastStallPos >= mid) {
+            cows++;
+            lastStallPos = arr[i];
+        }
+        if(cows == m) return true;
+    }
+    return false;
+}
 
+int posOfCows(vector<int> &arr, int n, int m)
+{
+    sort(arr.begin(), arr.end());
+    int st = 1, end = arr[n-1] - arr[0], ans = -1;
+    while (st <= end) 
+    {
+        int mid = st + (end - st)/2;
+        if(isPossible(arr, n, m, mid)) {
+            st = mid + 1;
+            ans = mid;
+        } else {
+            end = mid - 1;
+        }
+    }
+    return ans;
 }
 
 int main() {
-     vector<int> arr = {15, 17, 20};
-    int n = 3, m = 2;
+     vector<int> arr = {1,2,8,4,9};
+    int n = arr.size(), m = 3;
     cout << posOfCows(arr, n, m);
     return 0;
 }
