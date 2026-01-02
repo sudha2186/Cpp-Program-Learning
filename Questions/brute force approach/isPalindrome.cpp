612f5acb790e51016676158a9e1621d1d6fe1fb4
sudha2186 <rajendramaurya46223@gmail.com>
2026-01-02 23:48:25 +0530

    changes

--- Questions/brute force approach/isPalindrome.cpp
+++ Questions/brute force approach/isPalindrome.cpp
@@ -21,3 +21,22 @@ int main() {
     cout << isPalindrome(x);
     return 0;
 }
+
+// int reverse(int x) {
+//         int rev = 0;
+
+//         while (x != 0) {
+//             int digit = x % 10;
+//             x /= 10;
+//             if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && digit > 7)) return 0;
+//             if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && digit < -8)) return 0;
+
+//             rev = rev * 10 + digit;
+//         }
+//         return rev;
+//     } 
+//     bool isPalindrome(int x) {
+//         if(x < 0) return false;
+//         int revNum = reverse(x);
+//         return revNum == x;
+//     }
